338a3db9e57742061b42a12f61b25f8b58e52973
playtolearn-th <teacher.phairat@gmail.com>
2026-04-21 12:20:50 +0700

    Create server.js

---
+++ anyflip/server.js
@@ -0,0 +1,23 @@
+```json
+{
+  "name": "anyflip-downloader",
+  "version": "1.0.0",
+  "description": "AnyFlip to PDF Web App",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "pdfkit": "^0.14.0",
+    "puppeteer": "^21.5.0"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  }
+}
+
+
+```
